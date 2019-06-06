035cdc314bf246f8305b0784e0dcbb74b622dc06
ST-PC\ST <vanhongit@gmail.com>
2019-06-06 16:54:36 +0700

    create component and service

---
+++ angularNew/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ImagesComponent } from './images/images.component';
+import { ImageComponent } from './images/image/image.component';
+import { ImageListComponent } from './images/image-list/image-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ImagesComponent,
+    ImageComponent,
+    ImageListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
